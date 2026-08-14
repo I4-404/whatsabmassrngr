35a7c166e1d2e0d45ec2b43fae7b502d1da25115
I4-404 <wwtusr334@gmail.com>
2026-08-14 10:19:26 +0300

    Create Color.kt for theme color definitions
    Added color definitions for the app's theme.

---
+++ app/src/main/java/com/aa/autoresponder/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.aa.autoresponder.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// هوية بصرية بنفسجي/نيلي دافئ - مناسبة لتطبيق محادثات
+val Violet40 = Color(0xFF6750D8)
+val Violet80 = Color(0xFFC9BFFF)
+val VioletContainerLight = Color(0xFFE8E1FF)
+val VioletContainerDark = Color(0xFF4A3D9E)
+
+val Teal40 = Color(0xFF1F8A70)
+val Teal80 = Color(0xFF8DE8C9)
+
+val BgLight = Color(0xFFFBF8FF)
+val BgDark = Color(0xFF141218)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1B24)
+val SurfaceVariantLight = Color(0xFFF1ECFA)
+val SurfaceVariantDark = Color(0xFF2A2632)
+
+val ErrorLight = Color(0xFFBA1A1A)
+val ErrorDark = Color(0xFFFFB4AB)
